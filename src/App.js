00dfd9b417d3a038795668f0200f4522492c2bf2
Robert Kisiel <rkisiel@squiz.pl>
2019-10-09 10:44:36 +0200

    little cleanup and v15

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          v14 version
+          v15 version
         </p>
       </header>
     </div>

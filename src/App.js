b0359246fc5a7455a1c8fbf84d461acb23dc04bb
Robert Kisiel <rkisiel@squiz.pl>
2019-10-10 07:40:37 +0200

    V17

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          v16 version
+          v17 version
         </p>
       </header>
     </div>
